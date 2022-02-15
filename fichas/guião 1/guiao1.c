dd29a0ceb4d4530c94e7b97b76c22ca21a5a25e2
ritarded <ritalino7@hotmail.com>
2022-02-15 09:02:27 +0000

    nao vejam o que fiz antes lol

--- fichas/guião 1/guiao1.c
+++ fichas/guião 1/guiao1.c
@@ -1,13 +1,7 @@
 #include <sys/types.h> 
 #include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */ 
 #include <fcntl.h> /* O_RDONLY, O_WRONLY, O_CREAT, O_* */ 
-
-int open(const char *path, int oflag [, mode]); 
-ssize_t read(int fildes, void *buf, size_t nbyte); 
-ssize_t write(int fildes, const void *buf, size_t nbyte); 
-off_t lseek(int fd, off_t offset, int whence); 
-int close(int fildes); 
-
+#include <stdio.h>
 
 int main(int argc, char** argv){
     char* source = argv[1];
@@ -16,5 +10,5 @@ int main(int argc, char** argv){
     char buf;
     ssize_t nrd = read(fd_source, &buf, sizeof(char)); // nrd = number of bytes read
     close(fd_source);
-    printf("%c", buf);
+    printf("%c\n", buf);
 }
