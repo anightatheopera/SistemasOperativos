6f98a5b4a74b72b091d9d095f9ee46f63fccf724
ritarded <ritalino7@hotmail.com>
2022-07-12 20:05:15 +0100

    fixed some possible errors (?) i think its better now

--- exames e testes/exame 2021/exercicio2.c
+++ exames e testes/exame 2021/exercicio2.c
@@ -8,42 +8,42 @@ int round(char* cmd){
     int finished = 0;
     pid_t children[100];
 
+    /* setup an alarm signal for the next 20 secs */
+    signal(alarm_handler, SIG_ALARM);
+
     /* spawn children */
     for(int i = 0; i < 100; i++){
         children[i] = fork();
         if(children[i] == 0){
+            alarm(20);
             execlp(cmd, cmd, NULL);
         }
-  }
-  
-  /* setup an alarm signal for the next 20 secs */
-  signal(alarm_handler, SIG_ALARM);
-  alarm(20);
-
-  /* await until all finished or alarm has occurred */
-  while(finished < 100 && !alarm_occurred) {
-        pid_t pid = wait(NULL);
-        finished++;
-  
-        for(int i = 0; i < 100; i++){
-            if(children[i] == pid) {
-                children[i] = 0;
+    }
+    
+    /* await until all finished or alarm has occurred */
+    while(finished < 100 && !alarm_occurred) {
+            pid_t pid = wait(NULL);
+            finished++;
+    
+            for(int i = 0; i < 100; i++){
+                if(children[i] == pid) {
+                    children[i] = 0;
+                }
             }
-        }
-  }
+    }
 
-  /* remove alarm schedule if present */
-  alarm(0);
-  alarm_occurred = false;
+    /* remove alarm schedule if present */
+    alarm(0);
+    alarm_occurred = false;
 
-  /* kill children remaining */
-  for(int i = 0; i < 100; i++){
-        if(children[i] != 0) {
-            kill(children[i], SIGINT);
-        }
-  }
+    /* kill children remaining */
+    for(int i = 0; i < 100; i++){
+            if(children[i] != 0) {
+                kill(children[i], SIGINT);
+            }
+    }
 
-}
+    }
 
 int correr(int n){
     int ret = 0;
